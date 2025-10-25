e2cfd3e137ee298c02e52db835d5f5543bc2a705
atolinol <ATOLINOL@HOTMAIL.COM>
2025-10-25 18:50:40 +0000

    Initial commit: java project

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * cukicahamas-23 - Java Implementation
+ * A simple Hello World program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to cukicahamas-23");
+        
+        // Simple calculation
+        int[] numbers = {1, 2, 3, 4, 5};
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        System.out.println("Sum of numbers: " + total);
+    }
+}
